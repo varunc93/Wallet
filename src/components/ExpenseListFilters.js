2bcca80eacb96a0fb32bc08654b8230d5ce70e07
VC93 <reachvc93@gmail.com>
2018-06-05 08:31:10 -0700

    Fixed focus issue for dates

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -35,36 +35,27 @@ export class ExpenseListFilters extends React.Component {
             <div className="content-container">
               <div className="input-group">
                 <div className="input-group__item">
-                  <input
-                    type="text"
-                    className="text-input"
-                    placeholder="Search expenses"
-                    value={this.props.filters.text}
-                    onChange={this.onTextChange}
-                  />
+                    <input className="text-input" placeholder="Search Expenses" type="text" value={this.props.filters.text} onChange={this.onTextChange}/>
+               </div>
+               <div className="input-group__item">
+                    <select className="select" value={this.props.filters.sortBy} onChange={this.onSortChange}>
+                        <option value="date">Date</option>
+                        <option value="amount">Amount</option>
+                    </select>
                 </div>
                 <div className="input-group__item">
-                  <select
-                    className="select"
-                    value={this.props.filters.sortBy}
-                    onChange={this.onSortChange}>
-                    <option value="date">Date</option>
-                    <option value="amount">Amount</option>
-                  </select>
-                </div>
-                <div className="input-group__item">
-                  <DateRangePicker
-                    startDate={this.props.filters.startDate}
-                    endDate={this.props.filters.endDate}
-                    onDatesChange={this.onDatesChange}
-                    focusedInput={this.state.calendarFocused}
-                    onFocusChange={this.onFocusChange}
-                    showClearDates={true}
-                    numberOfMonths={1}
-                    isOutsideRange={() => false}
-                    startDateId="start"
-                    endDateId="end"
-                  />
+                    <DateRangePicker
+                        startDate={this.props.filters.startDate}
+                        endDate={this.props.filters.endDate}
+                        onDatesChange={this.onDatesChange}
+                        focusedInput={this.state.focused}
+                        onFocusChange={this.onFocusChange}
+                        showClearDates={true}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false}
+                        startDateId="start"
+                        endDateId="end"
+                    />
                 </div>
               </div>
             </div>
